d4fcf65ddb4ede49076403ff1a071069cccb29c8
Jelani Jackson <jeljack86@gmail.com>
2021-08-05 18:27:34 -0400

    Getting current authenticated user

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import logo from './logo.svg';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import { API } from 'aws-amplify';
+import { API, Auth } from 'aws-amplify';
 import React, { useEffect, useState } from 'react';
 
 function App() {
   const [response, setResponse] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
     API.get("algoappapi", "/api", null).then((data) => {
@@ -14,11 +15,18 @@ function App() {
     });
   }, []);
 
+  useEffect(() => {
+    Auth.currentAuthenticatedUser().then((user) => {
+      setCurrentUser(user);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have auth</h1>
+        {currentUser && <div>{currentUser}</div>}
         {response && <div>Response: {response}</div>}
       </header>
       <AmplifySignOut />
