1573f1daeab4156e42a1ef3b1b970ad9f9e02883
Jelani Jackson <jeljack86@gmail.com>
2021-08-05 09:06:08 -0400

    Updated all of the things

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import './App.css';
 import logo from './logo.svg';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
-import React from 'react';
+import { API } from 'aws-amplify';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [response, setResponse] = useState(null);
+
+  useEffect(() => {
+    API.get("algotraderappapi", "/api", null).then((data) => {
+      console.log(data);
+      setResponse(data);
+    });
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>We now have auth</h1>
+        {response && <div>Response: {response}</div>}
       </header>
       <AmplifySignOut />
     </div>
