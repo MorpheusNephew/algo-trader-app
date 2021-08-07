65c5da6a7599049ac7c777957cd0036ac9ee1093
Jelani Jackson <jeljack86@gmail.com>
2021-08-07 18:11:26 -0400

    Added checkUsername middleware

--- amplify/backend/function/algotraderappapi/lib/index.ts
+++ amplify/backend/function/algotraderappapi/lib/index.ts
@@ -1,8 +1,9 @@
+import checkUsername from './middleware/checkUsername';
 import { router } from './routes';
 import cors from '@koa/cors';
 import Koa from 'koa';
 import ServerlessHttp from 'serverless-http';
 
-const app = new Koa().use(cors()).use(router.routes());
+const app = new Koa().use(cors()).use(checkUsername).use(router.routes());
 
 export const handler = ServerlessHttp(app);
