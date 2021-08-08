d6111df5d6d5ed87ef94bf5db6343f210d8f5712
Jelani Jackson <jeljack86@gmail.com>
2021-08-08 08:38:50 -0400

    Added utility to convert UserType to AuthenticatedUser

---
+++ amplify/backend/function/algotraderappapi/lib/utils.ts
@@ -0,0 +1,16 @@
+import { AuthenticatedUser } from './types';
+import { UserType } from 'aws-sdk/clients/cognitoidentityserviceprovider';
+
+export const convertToAuthenticatedUser = (
+  user: UserType
+): AuthenticatedUser => {
+  const authenticatedUser: AuthenticatedUser = {
+    username: user.Username,
+  };
+
+  user.Attributes.forEach((attribute) => {
+    authenticatedUser[attribute.Name] = attribute.Value;
+  });
+
+  return authenticatedUser;
+};
