40e4367030005444128426c2a4f30350fbde9a18
Jelani Jackson <jeljack86@gmail.com>
2021-08-09 19:27:21 -0400

    Ran prettier

--- src/App.tsx
+++ src/App.tsx
@@ -5,33 +5,35 @@ import { API, Auth } from 'aws-amplify';
 import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [response, setResponse] = useState(null);
-  const [currentUser, setCurrentUser] = useState<any>(null);
+    const [response, setResponse] = useState(null);
+    const [currentUser, setCurrentUser] = useState<any>(null);
 
-  useEffect(() => {
-    API.get("algoappapi", "/api", null).then((data) => {
-      console.log(data);
-      setResponse(data);
-    });
-  }, []);
+    useEffect(() => {
+        API.get('algoappapi', '/api', null).then((data) => {
+            console.log(data);
+            setResponse(data);
+        });
+    }, []);
 
-  useEffect(() => {
-    Auth.currentAuthenticatedUser().then((user) => {
-      console.log("Authenticated user", user);
-      setCurrentUser(user);
-    });
-  }, []);
+    useEffect(() => {
+        Auth.currentAuthenticatedUser().then((user) => {
+            console.log('Authenticated user', user);
+            setCurrentUser(user);
+        });
+    }, []);
 
-  return (
-    <div className="App">
-      <Header />
-      <header>
-        <h1>We now have auth</h1>
-        {currentUser?.username && <div>Welcome {currentUser.username}</div>}
-        {response && <div>Response: {response}</div>}
-      </header>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <Header />
+            <header>
+                <h1>We now have auth</h1>
+                {currentUser?.username && (
+                    <div>Welcome {currentUser.username}</div>
+                )}
+                {response && <div>Response: {response}</div>}
+            </header>
+        </div>
+    );
 }
 
 export default withAuthenticator(App);
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,13 +2,13 @@ import { AmplifySignOut } from '@aws-amplify/ui-react';
 import { AppBar, Toolbar } from '@material-ui/core';
 
 const Header = () => (
-  <AppBar position="sticky">
-    <Toolbar>
-      <div>Header</div>
-      &nbsp;
-      <AmplifySignOut />
-    </Toolbar>
-  </AppBar>
+    <AppBar position='sticky'>
+        <Toolbar>
+            <div>Header</div>
+            &nbsp;
+            <AmplifySignOut />
+        </Toolbar>
+    </AppBar>
 );
 
 export default Header;
