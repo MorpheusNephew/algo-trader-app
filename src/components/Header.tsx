1cdb0939cc6d6966466eeccbe1ef6217c94b2755
Jelani Jackson <jeljack86@gmail.com>
2021-08-09 19:00:09 -0400

    Updated spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,8 @@ import { AppBar, Toolbar } from '@material-ui/core';
 const Header = () => (
   <AppBar position="sticky">
     <Toolbar>
-    <div>Header</div>
-    &nbsp;
+      <div>Header</div>
+      &nbsp;
       <AmplifySignOut />
     </Toolbar>
   </AppBar>
