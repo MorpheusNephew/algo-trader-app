6db5433d9ce6cc23270bf1ac592989ee4f3286cc
Jelani Jackson <jeljack86@gmail.com>
2021-08-09 20:50:56 -0400

    Updated checkUser to use new config

--- amplify/backend/function/algotraderappapi/lib/middleware/checkUser.ts
+++ amplify/backend/function/algotraderappapi/lib/middleware/checkUser.ts
@@ -1,42 +1,39 @@
+import config from '../config';
 import { convertToAuthenticatedUser } from '../utils';
 import { CognitoIdentityServiceProvider } from 'aws-sdk';
 import { Next } from 'koa';
 
 const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider({
-    apiVersion: '2016-04-18',
+  apiVersion: '2016-04-18',
 });
 
-const USER_POOL_ID = process.env.AUTH_ALGOTRADERAPP7860B9F7_USERPOOLID;
-
 const checkUser = async (ctx: any, next: Next) => {
-    const cognitoAuthenticationProvider =
-        ctx.req.requestContext?.identity?.cognitoAuthenticationProvider.split(
-            ','
-        );
+  const cognitoAuthenticationProvider =
+    ctx.req.requestContext?.identity?.cognitoAuthenticationProvider.split(',');
 
-    const userInfo = cognitoAuthenticationProvider[1].split(':');
-    const userSub = userInfo[userInfo.length - 1];
+  const userInfo = cognitoAuthenticationProvider[1].split(':');
+  const userSub = userInfo[userInfo.length - 1];
 
-    const request: CognitoIdentityServiceProvider.ListUsersRequest = {
-        UserPoolId: USER_POOL_ID,
-        Filter: `sub = "${userSub}"`,
-    };
+  const request: CognitoIdentityServiceProvider.ListUsersRequest = {
+    UserPoolId: config.cognitoUserPoolId,
+    Filter: `sub = "${userSub}"`,
+  };
 
-    const response = await cognitoIdentityServiceProvider
-        .listUsers(request)
-        .promise();
+  const response = await cognitoIdentityServiceProvider
+    .listUsers(request)
+    .promise();
 
-    const authenticatedUser = response.Users.map(convertToAuthenticatedUser)[0];
+  const authenticatedUser = response.Users.map(convertToAuthenticatedUser)[0];
 
-    if (!authenticatedUser) {
-        ctx.throw('User not found', 404);
-    }
+  if (!authenticatedUser) {
+    ctx.throw('User not found', 404);
+  }
 
-    console.log('User', JSON.stringify(authenticatedUser));
+  console.log('User', JSON.stringify(authenticatedUser));
 
-    ctx.state.authenticatedUser = authenticatedUser;
+  ctx.state.authenticatedUser = authenticatedUser;
 
-    await next();
+  await next();
 };
 
 export default checkUser;
