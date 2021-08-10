782447ea798b06cb25380e135d80a3ec785750c5
Jelani Jackson <jeljack86@gmail.com>
2021-08-09 21:21:52 -0400

    Updated checkUser to use getCognitoUser

--- amplify/backend/function/algotraderappapi/lib/middleware/checkUser.ts
+++ amplify/backend/function/algotraderappapi/lib/middleware/checkUser.ts
@@ -1,29 +1,8 @@
-import config from '../config';
-import { convertToAuthenticatedUser } from '../utils';
-import { CognitoIdentityServiceProvider } from 'aws-sdk';
+import { getCognitoUser } from '../services/cognito';
 import { Next } from 'koa';
 
-const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider({
-  apiVersion: '2016-04-18',
-});
-
 const checkUser = async (ctx: any, next: Next) => {
-  const cognitoAuthenticationProvider =
-    ctx.req.requestContext?.identity?.cognitoAuthenticationProvider.split(',');
-
-  const userInfo = cognitoAuthenticationProvider[1].split(':');
-  const userSub = userInfo[userInfo.length - 1];
-
-  const request: CognitoIdentityServiceProvider.ListUsersRequest = {
-    UserPoolId: config.cognitoUserPoolId,
-    Filter: `sub = "${userSub}"`,
-  };
-
-  const response = await cognitoIdentityServiceProvider
-    .listUsers(request)
-    .promise();
-
-  const authenticatedUser = response.Users.map(convertToAuthenticatedUser)[0];
+  const authenticatedUser = await getCognitoUser(ctx);
 
   if (!authenticatedUser) {
     ctx.throw('User not found', 404);
