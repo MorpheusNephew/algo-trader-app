19f704439573ede0ced2844fd6bf12effebcdff8
Jelani Jackson <jeljack86@gmail.com>
2021-08-23 13:25:28 -0400

    Added comments for adding connections

--- amplify/backend/function/algotraderappapi/lib/routes/connectionsRouter.ts
+++ amplify/backend/function/algotraderappapi/lib/routes/connectionsRouter.ts
@@ -4,6 +4,7 @@ import { Next } from 'koa';
 
 const connectionsRouter = new Router({ prefix: '/connections' })
   .get('Get connections', '/', async (ctx: AppContext, next: Next) => {
+    // Get connections for a user
     ctx.status = 200;
     ctx.body = JSON.stringify(
       `Getting all of ${ctx.state.authenticatedUser.username}'s connections`
@@ -12,6 +13,15 @@ const connectionsRouter = new Router({ prefix: '/connections' })
     await next();
   })
   .post('Creating connection', '/', async (ctx: AppContext, next: Next) => {
+    // Get `IConnection` from ctx.request.body
+
+    // Construct id and sortname
+
+    // save connection
+
+    // get saved connection
+
+    // return `IConnectionResponse`
     ctx.status = 201;
     ctx.body = JSON.stringify(
       `Creating connection for ${ctx.state.authenticatedUser.username}`
@@ -20,10 +30,13 @@ const connectionsRouter = new Router({ prefix: '/connections' })
     await next();
   })
   .del('Delete connection', '/:id', async (ctx: AppContext, next: Next) => {
+    // Get connection to delete
+
+    // if connection doesn't exist return a 404
+
+    // if it does continue to delete
+
     ctx.status = 204;
-    ctx.body = JSON.stringify(
-      `Deleting connection id (${ctx.params.id}) for ${ctx.state.authenticatedUser.username}`
-    );
 
     await next();
   });
