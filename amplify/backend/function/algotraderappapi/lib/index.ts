03302c8cfdc9960e20316eb1d2409842b1991f37
Jelani Jackson <jeljack86@gmail.com>
2021-08-23 09:32:56 -0400

    Added body parser to parse json

--- amplify/backend/function/algotraderappapi/lib/index.ts
+++ amplify/backend/function/algotraderappapi/lib/index.ts
@@ -2,8 +2,13 @@ import checkUser from './middleware/checkUser';
 import router from './routes';
 import cors from '@koa/cors';
 import Koa from 'koa';
+import bodyParser from 'koa-bodyparser';
 import ServerlessHttp from 'serverless-http';
 
-const app = new Koa().use(cors()).use(checkUser).use(router.routes());
+const app = new Koa()
+  .use(cors())
+  .use(bodyParser())
+  .use(checkUser)
+  .use(router.routes());
 
 export const handler = ServerlessHttp(app);
