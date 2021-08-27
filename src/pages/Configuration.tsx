b06bfe8293bcb946c6871b42cc389b2978acbe02
Jelani Jackson <jeljack86@gmail.com>
2021-08-26 20:23:53 -0400

    Updating page directory to be able to use the proper casing

--- src/pages/Configuration.tsx
+++ src/pages/Configuration.tsx
@@ -1,14 +1,11 @@
-import ConnectedBrokerages from '../components/ConnectedBrokerages';
-import TdAmeritrade from './Td';
+import TdAmeritrade from './Cool';
 
 const Configuration = () => (
   <div>
     <header>
       <h1>Configuration</h1>
     </header>
-    <ConnectedBrokerages>
-      <TdAmeritrade />
-    </ConnectedBrokerages>
+    <TdAmeritrade />
   </div>
 );
 
