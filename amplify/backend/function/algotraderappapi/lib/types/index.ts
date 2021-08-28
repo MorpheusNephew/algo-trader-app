7e6362c44fcd052d4046c3fab43e53ef436647f2
Jelani Jackson <jeljack86@gmail.com>
2021-08-28 06:16:13 -0400

    Added config to AppContextState

--- amplify/backend/function/algotraderappapi/lib/types/index.ts
+++ amplify/backend/function/algotraderappapi/lib/types/index.ts
@@ -1,14 +1,16 @@
+import { IConfig } from '../config';
 import { Context, DefaultState } from 'koa';
 
 export interface AppContext extends Context {
-    state: AppContextState;
+  state: AppContextState;
 }
 
 export interface AppContextState extends DefaultState {
-    authenticatedUser: AuthenticatedUser;
+  authenticatedUser: AuthenticatedUser;
+  config: IConfig;
 }
 
 export interface AuthenticatedUser {
-    username: string;
-    [key: string]: any;
+  username: string;
+  [key: string]: any;
 }
