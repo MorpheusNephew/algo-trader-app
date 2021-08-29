103f47d0d01cdc946ca0d2488c1c826be160d9a8
Jelani Jackson <jeljack86@gmail.com>
2021-08-29 16:32:56 -0400

    Moved Router to index

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ import Configuration from './pages/Configuration';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import { API } from 'aws-amplify';
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Route, Switch, useRouteMatch } from 'react-router-dom';
 
 function App() {
   const [response, setResponse] = useState(null);
   const currentUser = GetAuthenticatedUser();
+  const connectRoute = useRouteMatch({ path: '/connect' });
 
   useEffect(() => {
     API.get('algoappapi', '/api', null).then((data) => {
@@ -19,25 +20,21 @@ function App() {
   }, []);
 
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Switch>
-          <Route path='/configuration'>
-            <Configuration />
-          </Route>
-          <Route path='/'>
-            <header>
-              <h1>We now have auth</h1>
-              {currentUser?.username && (
-                <div>Welcome {currentUser.username}</div>
-              )}
-              {response && <div>Response: {response}</div>}
-            </header>
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className='App'>
+      {!!!connectRoute && <Header />}
+      <Switch>
+        <Route path='/configuration'>
+          <Configuration />
+        </Route>
+        <Route path='/'>
+          <header>
+            <h1>We now have auth</h1>
+            {currentUser?.username && <div>Welcome {currentUser.username}</div>}
+            {response && <div>Response: {response}</div>}
+          </header>
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
