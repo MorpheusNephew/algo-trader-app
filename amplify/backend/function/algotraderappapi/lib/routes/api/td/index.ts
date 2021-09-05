b2bc6cfcef128b9d9c0dd4d123ccdd86854a077c
Jelani Jackson <jeljack86@gmail.com>
2021-09-05 09:35:00 -0400

    Removed unused router

--- amplify/backend/function/algotraderappapi/lib/routes/api/td/index.ts
+++ amplify/backend/function/algotraderappapi/lib/routes/api/td/index.ts
@@ -1,9 +1,6 @@
 import { loadTdAmeritradeClient } from '../../../middleware/loadTdAmeritradeClient';
-import tdConnectionsRouter from './connections';
 import Router from '@koa/router';
 
-const tdRouter = new Router({ prefix: '/td' })
-  .use(loadTdAmeritradeClient)
-  .use(tdConnectionsRouter.routes());
+const tdRouter = new Router({ prefix: '/td' }).use(loadTdAmeritradeClient);
 
 export default tdRouter;
