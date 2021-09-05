4482d98c55ccc5065fbee678f47be4ce56ca948a
Jelani Jackson <jeljack86@gmail.com>
2021-09-05 09:50:26 -0400

    Updated parameter to _event

--- src/pages/Td/index.tsx
+++ src/pages/Td/index.tsx
@@ -1,12 +1,12 @@
 import { Add } from '@material-ui/icons';
 
-const openAuthWindow = (e: any, authUrl: string) => {
+const openAuthWindow = (_event: any, authUrl: string) => {
   window.open(authUrl, 'Auth', 'width=500,height=500');
   return false;
 };
 
 const ConnectButton = () => (
-  <div onClick={(e) => openAuthWindow(e, '/connect/td')}>
+  <div onClick={(event) => openAuthWindow(event, '/connect/td')}>
     <Add />
   </div>
 );
