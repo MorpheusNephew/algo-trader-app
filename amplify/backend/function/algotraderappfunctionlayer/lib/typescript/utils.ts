c0a022ae6f96d129f30592d4d1257576d5a9f22b
Jelani Jackson <jeljack86@gmail.com>
2021-09-13 08:56:50 -0400

    Added functionality for encrypting/decrypting items

--- amplify/backend/function/algotraderappfunctionlayer/lib/typescript/utils.ts
+++ amplify/backend/function/algotraderappfunctionlayer/lib/typescript/utils.ts
@@ -1,6 +1,27 @@
+import { Config } from './config';
+import Cryptr from 'cryptr';
+
 export const getDateSecondsFromNow = (seconds: number): string => {
   const currentDate = new Date();
   currentDate.setSeconds(currentDate.getSeconds() + seconds);
 
   return currentDate.toISOString();
+};
+
+const getCryptr = async () => {
+  const { cognitoUserPoolId } = await Config.getConfig();
+
+  return new Cryptr(cognitoUserPoolId);
+};
+
+export const encryptItem = async (itemToEncrypt: string): Promise<string> => {
+  const { encrypt } = await getCryptr();
+
+  return encrypt(itemToEncrypt);
+};
+
+export const decryptItem = async (itemToDecrypt: string): Promise<string> => {
+  const { decrypt } = await getCryptr();
+
+  return decrypt(itemToDecrypt);
 };
