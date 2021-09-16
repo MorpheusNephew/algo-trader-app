dacd1b6e19ca273cc6d9043a2ec4f7699a6e64d0
Jelani Jackson <jeljack86@gmail.com>
2021-09-16 19:54:33 -0400

    Added connection(s) to state

--- amplify/backend/function/algotraderappapi/lib/types.ts
+++ amplify/backend/function/algotraderappapi/lib/types.ts
@@ -1,5 +1,6 @@
 import TdAmeritradeClient from '@morpheusnephew/td-ameritrade/dist/clients';
 import { IConfig } from '/opt/nodejs/config';
+import { IConnection } from '/opt/nodejs/connectionTypes';
 import { Context, DefaultState } from 'koa';
 
 export interface AppContext extends Context {
@@ -10,6 +11,8 @@ export interface AppContextState extends DefaultState {
   authenticatedUser: AuthenticatedUser;
   config: IConfig;
   tdAmeritradeClient: TdAmeritradeClient;
+  connection: IConnection;
+  connections: IConnection[];
 }
 
 export interface AuthenticatedUser {
