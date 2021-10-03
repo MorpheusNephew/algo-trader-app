a09fdd587a0a15f263afa77037bbbee1bc741de2
Jelani Jackson <jeljack86@gmail.com>
2021-10-03 06:41:12 -0400

    Getting data and status from client response

--- amplify/backend/function/algotraderappapi/lib/routes/api/td/accounts.ts
+++ amplify/backend/function/algotraderappapi/lib/routes/api/td/accounts.ts
@@ -6,27 +6,22 @@ export const tdAccountsRouter = new Router({ prefix: '/accounts' })
   .get('/', async (ctx: AppContext, _next: Next) => {
     const { fields } = ctx.query;
 
-    const accounts = await ctx.state.tdAmeritradeClient.accounts.getAllAccounts(
-      fields as any
-    );
+    const { data, status } =
+      await ctx.state.tdAmeritradeClient.accounts.getAllAccounts(fields as any);
 
-    ctx.status = 200;
-    ctx.body = JSON.stringify(accounts);
+    ctx.status = status;
+    ctx.body = JSON.stringify(data);
   })
   .get('/:accountId', async (ctx: AppContext, _next: Next) => {
     const { accountId } = ctx.params;
     const { fields } = ctx.query;
 
-    const account = await ctx.state.tdAmeritradeClient.accounts.getAccount(
-      accountId,
-      fields as any
-    );
+    const { data, status } =
+      await ctx.state.tdAmeritradeClient.accounts.getAccount(
+        accountId,
+        fields as any
+      );
 
-    if (!account) {
-      ctx.status = 404;
-      ctx.body = JSON.stringify(`Account (${accountId}) not found`);
-    } else {
-      ctx.status = 200;
-      ctx.body = JSON.stringify(account);
-    }
+    ctx.status = status;
+    ctx.body = JSON.stringify(data);
   });
