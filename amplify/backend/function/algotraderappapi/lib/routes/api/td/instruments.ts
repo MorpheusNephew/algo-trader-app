5fae48f3a3697810c98897bc3d8cde2b6e7aa497
Jelani Jackson <jeljack86@gmail.com>
2021-10-03 22:08:08 -0400

    Instruments endpoints added

--- amplify/backend/function/algotraderappapi/lib/routes/api/td/instruments.ts
+++ amplify/backend/function/algotraderappapi/lib/routes/api/td/instruments.ts
@@ -1,3 +1,27 @@
+import { AppContext } from '../../../types';
 import Router from '@koa/router';
+import { Projection } from '@morpheusnephew/td-ameritrade/dist/clients/instruments-client';
+import { Next } from 'koa';
 
-export const tdInstrumentsRouter = new Router({ prefix: '/instruments' });
+export const tdInstrumentsRouter = new Router({ prefix: '/instruments' })
+  .get('Get instruments', '/', async (ctx: AppContext, _next: Next) => {
+    const { symbol, projection } = ctx.query;
+
+    const { data, status } =
+      await ctx.state.tdAmeritradeClient.instruments.getInstruments(
+        symbol as string,
+        projection as Projection
+      );
+
+    ctx.status = status;
+    ctx.body = JSON.stringify(data);
+  })
+  .get('Get instrument', '/:cusip', async (ctx: AppContext, _next: Next) => {
+    const { cusip } = ctx.params;
+
+    const { data, status } =
+      await ctx.state.tdAmeritradeClient.instruments.getInstrument(cusip);
+
+    ctx.status = status;
+    ctx.body = JSON.stringify(data);
+  });
