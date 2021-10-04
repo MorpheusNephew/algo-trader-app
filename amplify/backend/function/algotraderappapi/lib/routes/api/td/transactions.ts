e4e4f5e3589e1a3157ec2982880baa3a206404d0
Jelani Jackson <jeljack86@gmail.com>
2021-10-03 22:44:56 -0400

    Implemented transaction endpoints

--- amplify/backend/function/algotraderappapi/lib/routes/api/td/transactions.ts
+++ amplify/backend/function/algotraderappapi/lib/routes/api/td/transactions.ts
@@ -1,3 +1,41 @@
+import { AppContext } from '../../../types';
 import Router from '@koa/router';
+import { Next } from 'koa';
 
-export const tdTransactionsRouter = new Router({ prefix: '/transactions' });
+export const tdTransactionsRouter = new Router({ prefix: '/transactions' })
+  .get(
+    'Get transactions',
+    '/:accountId',
+    async (ctx: AppContext, _next: Next) => {
+      const { accountId } = ctx.params;
+      const { type, symbol, startDate, endDate } = ctx.query;
+
+      const { data, status } =
+        await ctx.state.tdAmeritradeClient.transactionHistory.getTransactions(
+          accountId,
+          type as any,
+          symbol as string,
+          startDate as any,
+          endDate as any
+        );
+
+      ctx.status = status;
+      ctx.body = JSON.stringify(data);
+    }
+  )
+  .get(
+    'Get transaction',
+    '/:accountId/:transactionId',
+    async (ctx: AppContext, _next: Next) => {
+      const { accountId, transactionId } = ctx.params;
+
+      const { data, status } =
+        await ctx.state.tdAmeritradeClient.transactionHistory.getTransaction(
+          accountId,
+          transactionId
+        );
+
+      ctx.status = status;
+      ctx.body = JSON.stringify(data);
+    }
+  );
