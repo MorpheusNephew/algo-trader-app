431625b7b81200aaf95d095ee8438ccb7c88ad66
Jelani Jackson <jeljack86@gmail.com>
2021-10-12 13:41:12 -0700

    Make other changes for Account Information

--- src/components/td/AccountInformation.tsx
+++ src/components/td/AccountInformation.tsx
@@ -1,21 +1,29 @@
 import { getAccountsInformation } from '../../clients/td';
 import { Typography } from '@material-ui/core';
-import { Account } from '@morpheusnephew/td-ameritrade-models';
 import { useEffect, useState } from 'react';
+import {
+  CashAccount,
+  MarginAccount,
+} from '@morpheusnephew/td-ameritrade-models';
 
 interface IInput {
   hasConnection: boolean;
 }
 
 export const AccountInformation = (params: IInput) => {
-  const [account, setAccount] = useState<Account>();
+  const [account, setAccount] = useState<CashAccount | MarginAccount>();
   const { hasConnection } = params;
 
   useEffect(() => {
     if (hasConnection) {
       getAccountsInformation().then((data) => {
-        const accountInformation = data[0];
-        setAccount(accountInformation);
+        const { securitiesAccount } = data[0];
+
+        if (securitiesAccount?.type === 'CASH') {
+          setAccount(securitiesAccount as CashAccount);
+        } else if (securitiesAccount?.type === 'MARGIN') {
+          setAccount(securitiesAccount as MarginAccount);
+        }
       });
     }
   }, [hasConnection]);
@@ -23,11 +31,7 @@ export const AccountInformation = (params: IInput) => {
   return (
     <div>
       <Typography>Account Information</Typography>
-      {account && (
-        <Typography>
-          {JSON.stringify(account.securitiesAccount?.accountId)}
-        </Typography>
-      )}
+      {account && <Typography>{JSON.stringify(account)}</Typography>}
     </div>
   );
 };
