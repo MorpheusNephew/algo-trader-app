93567bed83a3f6456df7c48680f0a085896dc914
Jelani Jackson <jeljack86@gmail.com>
2021-10-17 17:39:09 -0400

    Updated console logs to use logger

--- amplify/backend/function/tdrefreshtokenrefresher/lib/index.ts
+++ amplify/backend/function/tdrefreshtokenrefresher/lib/index.ts
@@ -2,11 +2,14 @@ import TdAmeritradeClient from '@morpheusnephew/td-ameritrade/dist/clients';
 import { Config } from '/opt/nodejs/config';
 import { IConnection, TConnection } from '/opt/nodejs/connectionTypes';
 import { convertTokenToIConnection } from '/opt/nodejs/connectionUtils';
+import Logger from '/opt/nodejs/logger';
 import { differenceInCalendarDays, parseISO } from 'date-fns';
 import {
   getConnections,
   updateConnectionTokens,
 } from '/opt/nodejs/connectiondb';
+
+const logger = Logger.getLogger('refresh-token-refresher');
 
 const connectionType: TConnection = 'td';
 
@@ -28,7 +31,7 @@ export const handler = async (_event: any) => {
         clientId: tdConsumerKey,
       });
 
-      console.log(`Updating refresh token for: ${username}`);
+      logger.info('Updated refresh token for user', { username, connectionId });
 
       const { data: tokenResponse } = await client.auth.refreshRefreshToken();
 
@@ -40,7 +43,10 @@ export const handler = async (_event: any) => {
 
       await updateConnectionTokens(username, connectionId, connectionToUpdate);
 
-      console.log(`Refresh token for ${username} has been updated`);
+      logger.info('Refresh token for user has been updated', {
+        username,
+        connectionId,
+      });
     }
 
     return true;
