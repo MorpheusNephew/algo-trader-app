e23a201c073194e34675c3fd367e69fff53381b0
Jelani Jackson <jeljack86@gmail.com>
2021-10-17 16:12:15 -0400

    Using the console to write exception handlers

--- amplify/backend/function/algotraderappfunctionlayer/lib/typescript/logger.ts
+++ amplify/backend/function/algotraderappfunctionlayer/lib/typescript/logger.ts
@@ -2,4 +2,5 @@ import winston from 'winston';
 
 export const logger = winston.createLogger({
   transports: [new winston.transports.Console()],
+  exceptionHandlers: [new winston.transports.Console()],
 });
