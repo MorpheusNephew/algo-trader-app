20cea817df01e436f5dc386c13981708a02205c0
Jelani Jackson <jeljack86@gmail.com>
2021-10-17 23:36:36 -0400

    Add log messages

--- amplify/backend/function/algotraderappapi/lib/middleware/loadConfig.ts
+++ amplify/backend/function/algotraderappapi/lib/middleware/loadConfig.ts
@@ -2,8 +2,14 @@ import { AppContext } from '../types';
 import { Config } from '/opt/nodejs/config';
 import { Next } from 'koa';
 
+const loggerOptions = {
+  fileName: 'loadConfig.ts',
+};
+
 export const loadConfig = async (ctx: AppContext, next: Next) => {
+  ctx.state.logger.info('Loading config', loggerOptions);
   ctx.state.config = await Config.getConfig();
+  ctx.state.logger.info('Config loaded', loggerOptions);
 
   await next();
 };
--- amplify/backend/function/algotraderappapi/lib/middleware/loadTdConnections.ts
+++ amplify/backend/function/algotraderappapi/lib/middleware/loadTdConnections.ts
@@ -2,17 +2,34 @@ import { AppContext } from '../types';
 import { getConnections } from '/opt/nodejs/connectiondb';
 import { Next } from 'koa';
 
+const loggerOptions = {
+  fileName: 'loadTdConnections.ts',
+};
+
 export const loadTdConnections = async (ctx: AppContext, next: Next) => {
   const {
     authenticatedUser: { username },
+    logger,
   } = ctx.state;
+
+  logger.info('Loading TD connections', loggerOptions);
+  logger.info('Getting TD connections', loggerOptions);
 
   const connections = await getConnections({
     username,
     connectionType: 'td',
   });
 
-  ctx.state.connections = connections ?? [];
+  if (connections) {
+    ctx.state.connections = connections;
+    logger.info('Connections loaded', loggerOptions);
+  } else {
+    ctx.state.connections = [];
+    logger.info(
+      'Connections not found returning; Loading empty array',
+      loggerOptions
+    );
+  }
 
   await next();
 };
