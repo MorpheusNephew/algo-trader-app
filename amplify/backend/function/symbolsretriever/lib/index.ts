d69c47879c378ef7f33e05b0600979f0b6fb8f99
Jelani Jackson <jeljack86@gmail.com>
2021-11-07 11:47:24 -0500

    Updated symbolsRetriever to upsert symbols to db

--- amplify/backend/function/symbolsretriever/lib/index.ts
+++ amplify/backend/function/symbolsretriever/lib/index.ts
@@ -1,14 +1,11 @@
 import logger from './logger';
 import { getSymbolsUrl } from './nasdaq/urls';
 import { getCompaniesInfo } from './puppeteer';
+import { upsertCompaniesInfo } from '/opt/nodejs/companydb';
 
 export const handler = async (_event: any) => {
   logger.info('Getting ready to fill company symbols');
   const companiesInfo = await getCompaniesInfo(getSymbolsUrl());
 
-  logger.info('Companies info', {
-    companiesInfo: JSON.stringify(companiesInfo),
-  });
-
-  return;
+  await upsertCompaniesInfo(companiesInfo);
 };
