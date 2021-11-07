42e612024e21c0e5ccb3b9eb8fb67c889e8fef39
Jelani Jackson <jeljack86@gmail.com>
2021-11-07 11:45:28 -0500

    Updated upsertCompaniesInfo to take companies info to batchWrite in chunks of 25

--- amplify/backend/function/algotraderappfunctionlayer/lib/nodejs/companydb.js
+++ amplify/backend/function/algotraderappfunctionlayer/lib/nodejs/companydb.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.upsertCompanyInfo = void 0;
+exports.upsertCompaniesInfo = void 0;
 
 var _dynamodb = require("./dynamodb");
 
@@ -11,29 +11,49 @@ var _logger = _interopRequireDefault(require("./logger"));
 
 var _utilDynamodb = require("@aws-sdk/util-dynamodb");
 
+var _lodash = require("lodash");
+
+var _pMap = _interopRequireDefault(require("p-map"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { Promise.resolve(value).then(_next, _throw); } }
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-const upsertCompanyInfo = /*#__PURE__*/function () {
-  var _ref = _asyncToGenerator(function* () {
-    _logger.default.info('Upserting company info');
+const CHUNK = 25;
 
-    const input = {
-      Item: (0, _utilDynamodb.marshall)({
-        id: 'company',
-        sortName: 'Symbol',
-        companyName: 'Company Name'
-      })
-    };
-    return (0, _dynamodb.putItem)(input);
+const upsertCompaniesInfo = /*#__PURE__*/function () {
+  var _ref = _asyncToGenerator(function* (companiesInfo) {
+    _logger.default.info('Upserting companies info', {
+      companiesInfo
+    });
+
+    const companiesInfoChunks = (0, _lodash.chunk)(companiesInfo, CHUNK);
+
+    const upsertCompaniesInfoMapper = /*#__PURE__*/function () {
+      var _ref2 = _asyncToGenerator(function* (companies) {
+        const putRequest = companies.map(company => ({
+          Item: (0, _utilDynamodb.marshall)({
+            id: `company:${company.symbol}`,
+            sortName: company.symbol,
+            companyName: company.name
+          })
+        }));
+        return (0, _dynamodb.batchWriteItem)(putRequest);
+      });
+
+      return function upsertCompaniesInfoMapper(_x2) {
+        return _ref2.apply(this, arguments);
+      };
+    }();
+
+    return (0, _pMap.default)(companiesInfoChunks, upsertCompaniesInfoMapper);
   });
 
-  return function upsertCompanyInfo() {
+  return function upsertCompaniesInfo(_x) {
     return _ref.apply(this, arguments);
   };
 }();
 
-exports.upsertCompanyInfo = upsertCompanyInfo;
+exports.upsertCompaniesInfo = upsertCompaniesInfo;
--- amplify/backend/function/algotraderappfunctionlayer/lib/typescript/companydb.ts
+++ amplify/backend/function/algotraderappfunctionlayer/lib/typescript/companydb.ts
@@ -1,17 +1,29 @@
-import { putItem, TPutItemInput } from './dynamodb';
+import { batchWriteItem, TPutItemInput } from './dynamodb';
 import logger from './logger';
+import { CompanyInfo } from './types';
+import { PutRequest } from '@aws-sdk/client-dynamodb';
 import { marshall } from '@aws-sdk/util-dynamodb';
+import { chunk } from 'lodash';
+import pMap from 'p-map';
 
-export const upsertCompanyInfo = async () => {
-  logger.info('Upserting company info');
+const CHUNK = 25;
 
-  const input: TPutItemInput = {
-    Item: marshall({
-      id: 'company',
-      sortName: 'Symbol',
-      companyName: 'Company Name',
-    }),
+export const upsertCompaniesInfo = async (companiesInfo: CompanyInfo[]) => {
+  logger.info('Upserting companies info', { companiesInfo });
+
+  const companiesInfoChunks = chunk(companiesInfo, CHUNK);
+
+  const upsertCompaniesInfoMapper = async (companies: CompanyInfo[]) => {
+    const putRequest: PutRequest[] = companies.map((company) => ({
+      Item: marshall({
+        id: `company:${company.symbol}`,
+        sortName: company.symbol,
+        companyName: company.name,
+      }),
+    }));
+
+    return batchWriteItem(putRequest);
   };
 
-  return putItem(input);
+  return pMap(companiesInfoChunks, upsertCompaniesInfoMapper);
 };
