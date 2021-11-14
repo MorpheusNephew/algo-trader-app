9c3c79552498cf888139e38fd54e5d8cafd83c3a
Jelani Jackson <jeljack86@gmail.com>
2021-11-14 14:38:16 -0500

    Passing loggerOptions as object param

--- amplify/backend/function/algotraderappapi/lib/middleware/loadConfig.ts
+++ amplify/backend/function/algotraderappapi/lib/middleware/loadConfig.ts
@@ -8,9 +8,9 @@ const loggerOptions = {
 
 export const loadConfig = async (ctx: AppContext, next: Next) => {
   const { logger } = ctx.state;
-  logger.info('Loading config', loggerOptions);
+  logger.info('Loading config', { loggerOptions });
   ctx.state.config = await getConfig();
-  logger.info('Config loaded', loggerOptions);
+  logger.info('Config loaded', { loggerOptions });
 
   await next();
 };
