818ead99854fa3a0429cb55b5a0b7aa686ddfb96
Jelani Jackson <jeljack86@gmail.com>
2021-11-27 14:31:23 -0500

    Setting up websockets

--- src/pages/Td/index.tsx
+++ src/pages/Td/index.tsx
@@ -1,9 +1,12 @@
+import { getUserPrincipal } from '../../clients/td';
+import { getLoginRequest } from '../../clients/td/stream';
 import { AccountInformation } from '../../components/td/AccountInformation';
 import { Movers } from '../../components/td/Movers';
 import { useGetBrokerageConnections } from '../../hooks/useGetBrokerageConnections';
 import { Typography } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import { isEmpty } from 'lodash';
+import { useEffect } from 'react';
 
 const openAuthWindow = (_event: any, authUrl: string) => {
   window.open(authUrl, 'Auth', 'width=500,height=500');
@@ -19,6 +22,37 @@ const ConnectButton = () => (
 const TdAmeritrade = () => {
   const [tdConnections, loadingTdConnections] =
     useGetBrokerageConnections('td');
+
+  useEffect(() => {
+    getUserPrincipal().then((userPrincipal) => {
+      const loginRequest = getLoginRequest(userPrincipal);
+
+      console.log('Login Request', loginRequest);
+
+      const request = {
+        requests: [loginRequest],
+      };
+
+      const tdSocket = new WebSocket(
+        'wss://' + userPrincipal.streamerInfo!.streamerSocketUrl + '/ws'
+      );
+
+      tdSocket.onopen = (evt) => {
+        console.log('Socket opened', evt);
+      };
+
+      tdSocket.onmessage = (evt) => {
+        console.log(evt.data);
+      };
+      tdSocket.onclose = () => {
+        console.log('CLOSED');
+      };
+
+      (window as any).tdWebSocket = tdSocket;
+      (window as any).tdRequest = JSON.stringify(request);
+      (window as any).userDetails = userPrincipal;
+    });
+  }, []);
 
   return (
     <div>
