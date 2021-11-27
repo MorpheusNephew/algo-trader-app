4cc4d1e4f9d275a8ef09446593c84d1a902600a9
Jelani Jackson <jeljack86@gmail.com>
2021-11-27 13:59:44 -0500

    Added typing and updated method name

--- src/clients/td/user/index.ts
+++ src/clients/td/user/index.ts
@@ -1,10 +1,11 @@
 import { ALGO_API } from '../../types';
 import { UserPrincipalField } from './types';
+import { UserPrincipal } from '@morpheusnephew/td-ameritrade-models';
 import { API } from 'aws-amplify';
 
 const baseEndpoint = '/api/td/user';
 
-export const getUserDetails = async () => {
+export const getUserPrincipal = async (): Promise<UserPrincipal> => {
   const fields: UserPrincipalField[] = [
     'streamerSubscriptionKeys',
     'streamerConnectionInfo',
