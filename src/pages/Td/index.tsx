ed20342b3037a6b981f9e36f80f8d65de2c29a7f
Jelani Jackson <jeljack86@gmail.com>
2021-11-29 04:32:05 -0500

    Updated useGetBrokerageConnections location

--- src/pages/Td/index.tsx
+++ src/pages/Td/index.tsx
@@ -3,7 +3,7 @@ import { getLoginRequest } from '../../clients/td/stream';
 import { AccountInformation } from '../../components/td/AccountInformation';
 import { Movers } from '../../components/td/Movers';
 import { SymbolSelector } from '../../components/td/SymbolSelector';
-import { useGetBrokerageConnections } from '../../hooks/useGetBrokerageConnections';
+import { useGetBrokerageConnections } from '../../hooks/connections/useGetBrokerageConnections';
 import { Typography } from '@material-ui/core';
 import { Add } from '@material-ui/icons';
 import { isEmpty } from 'lodash';
@@ -60,14 +60,9 @@ const TdAmeritrade = () => {
       <Typography variant='h5' component='div'>
         TD AmeriTrade
       </Typography>
-      <Typography>
-        This is the page that will check whether a user has a TD Ameritrade
-        connection or not
-      </Typography>
       {(loadingTdConnections && <div>Loading...</div>) ||
         (isEmpty(tdConnections) && <ConnectButton />) || (
           <div>
-            <div>Td connected</div>
             <AccountInformation hasConnection={!isEmpty(tdConnections)} />
             <Movers />
             <SymbolSelector />
